1aff1fef14c285ca6948bd25e51837fed8cf9b0d
LucyKinyanjui <kinyanjuiwambui86@gmail.com>
2024-07-26 09:33:30 +0000

    taskit

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
-"""
-Encrypting passwords
+"""Module for encrypting passwords.
 """
 
 
+import logging
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """Hashes the provided password using bcrypt.
+
+    Use the bcrypt package to perform the hashing (with hashpw).
+
+    Args:
+        password (str): Password to be hashed.
+
+    Returns:
+        bytes: A salted, hashed password in byte string format.
     """
-    Salted pass generation
-    """
+    # Salt and hash the password using the bcrypt package
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ is valid?
+    """Validates that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): Hashed password.
+        password (str): Password to be validated.
+
+    Returns:
+        bool: True if the hashed password was formed from the given password,
+        otherwise False.
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # Try to match the hashed password with the given password
+    try:
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    # If there is an exception in the process, log the error message
+    except Exception as e:
+        logging.error("Error in password validation: {}".format(e))
+        return False
